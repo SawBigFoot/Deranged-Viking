cc6806445f665f273f98e227f2c1d4c9edabc64c
SawBigFoot <156308308+SawBigFoot@users.noreply.github.com>
2024-10-23 12:44:34 +0200

    Does not loop more then once for the change
    if (score >= 50) {
    
        if (loopconst) {
            loopconst = false
            console.log("passed 50" + scoreElem + "")
        }
    }

--- script.js
+++ script.js
@@ -62,15 +62,13 @@ scoreElem.textContent = Math.floor(score)
 console.log(score)
 console.log(delta)
 
-
-
-
-
-if (score >= 200) {
-    console.log("passed 50" + scoreElem + "")
+if (score >= 50) {
+    
+    if (loopconst) {
+        loopconst = false
+        console.log("passed 50" + scoreElem + "")
+    }
 }
-
-
 
 }
 
