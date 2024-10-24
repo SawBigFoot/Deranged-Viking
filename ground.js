690e7f0b0627291e43475c761e582bae9ebce40b
Maekiz <manoa039@osloskolen.no>
2024-10-24 13:57:11 +0200

    banner implemeted

--- ground.js
+++ ground.js
@@ -1,56 +1,45 @@
-
-
 import {
+  getCustomProperty,
+  incrementCustomProperty,
+  setCustomProperty,
+} from "./updateCustomProperty.js";
 
-getCustomProperty,
-
-incrementCustomProperty,
-
-setCustomProperty,
-
-} from "./updateCustomProperty.js"
-
-const SPEED = 0.05
-const groundElems = document.querySelectorAll("[data-ground]")
-var backgroundElems = document.querySelectorAll("img.background[data-background]");
+const SPEED = 0.05;
+const groundElems = document.querySelectorAll("[data-ground]");
+var backgroundElems = document.querySelectorAll(
+  "img.background[data-background]"
+);
 
 export function setupGround() {
-    setCustomProperty(groundElems[0], "--left", 0)
-    setCustomProperty(groundElems[1], "--left", 300)
+  setCustomProperty(groundElems[0], "--left", 0);
+  setCustomProperty(groundElems[1], "--left", 300);
 }
 
 export function setupBackGround() {
-    setCustomProperty(backgroundElems[0], "--left", 0)
-    setCustomProperty(backgroundElems[1], "--left", 300)
+  setCustomProperty(backgroundElems[0], "--left", 0);
+  setCustomProperty(backgroundElems[1], "--left", 300);
 }
-
 
 export function updateGround(delta, speedScale) {
-    
+  groundElems.forEach((ground) => {
+    incrementCustomProperty(ground, "--left", delta * speedScale * SPEED * -1);
 
-    groundElems.forEach(ground => {
-
-        incrementCustomProperty(ground, "--left", delta * speedScale * SPEED * -1)
-
-        if (getCustomProperty(ground, "--left") <= -300) {
-        incrementCustomProperty(ground, "--left", 600)
-        }
-
-
-}
-)
+    if (getCustomProperty(ground, "--left") <= -300) {
+      incrementCustomProperty(ground, "--left", 600);
+    }
+  });
 }
 
 export function updateBackGround(delta, speedScale) {
-    
-    backgroundElems.forEach(background => {
-    
-        incrementCustomProperty(background, "--left", delta * speedScale * SPEED * -1)
-    
-        if (getCustomProperty(background, "--left") <= -100) {
-        incrementCustomProperty(background, "--left", 100)
-        }
-    }
-    )    
+  backgroundElems.forEach((background) => {
+    incrementCustomProperty(
+      background,
+      "--left",
+      delta * speedScale * SPEED * -1
+    );
 
+    if (getCustomProperty(background, "--left") <= -100) {
+      incrementCustomProperty(background, "--left", 100);
+    }
+  });
 }
